bf5dd149470f744ac2524169a5e7bfca3b13242d
Mohamed Hashem <work@dadberg.com>
2021-04-28 02:13:38 +0200

    Update forms.py

--- web-app/forms.py
+++ web-app/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from flask_wtf import Form
-from wtforms import StringField, TextField, SubmitField, IntegerField,TextAreaField,RadioField,SelectField, DecimalField
+from wtforms import StringField, TextField, SubmitField, IntegerField,TextAreaField,RadioField,SelectField, DecimalField, FloatField
 from wtforms.validators import DataRequired
 from wtforms.validators import Length
 from wtforms.validators import ValidationError
 
 class PredictForm(FlaskForm):
-   mean_radius = IntegerField('Mean Radius')
-   mean_texture = StringField('Mean Texture')
-   mean_perimeter = DecimalField('Mean Perimeter')
-   mean_area = IntegerField('Mean Area')
-   mean_smoothness = StringField('Mean Smoothness')
+   mean_radius = FloatField('Mean Radius')
+   mean_texture = FloatField('Mean Texture')
+   mean_perimeter = FloatField('Mean Perimeter')
+   mean_area = FloatField('Mean Area')
+   mean_smoothness = FloatField('Mean Smoothness')
    submit = SubmitField('Predict')
    abc = "" # this variable is used to send information back to the front page
